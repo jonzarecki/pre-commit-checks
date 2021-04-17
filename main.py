7345f73ef026c6ad1aba15e29741fa6256686750
Jonathan Zarecki <yonilx@gmail.com>
2021-04-17 11:37:19 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+Main class for main things
+"""
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def very_nice_function() -> None:
+	"""
+
+	:rtype: None
+	"""
+	print("hello world")
+
+
+def print_hi(name: str):
+	# Use a breakpoint in the code line below to debug your script.
+	print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+	print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ module/class.py
@@ -0,0 +1,9 @@
+"""
+Example class for nice things
+"""
+
+
+class Tasdasdasdasdasdasdasdasd:
+	a = 1
+	b = 2
+	c = 3
