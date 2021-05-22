ad49f2fbb02c2e5a860b139de04b8316159bea4b
Jonathan Zarecki <yonilx@gmail.com>
2021-05-22 16:26:52 +0300

    test: check pre-commit hook

---
+++ notebooks/example_note.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+
+a = 1
+print(a)
+
+# %% pycharm={"name": "#%%\n"}
+print("hello world")
+asaa = 2
+
+# %%
+b = 2
+a22 = 31
+
+new_param = 3
+
+# %%
+c = 5
+
+# %%
