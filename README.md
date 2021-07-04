ede3e3cef4ce2280c4c02e6cfa1813d983ab09dc
Jonathan Zarecki <yonilx@gmail.com>
2021-07-05 00:24:41 +0300

    ci: add new configs and checks from "checks" branch
    add new configs and checks from "checks" branch

---
+++ README.md
@@ -0,0 +1,6 @@
+## Development
+To enable pre-commit hooks run (in the main project folder)
+
+```bash
+pre-commit install --install-hooks -t pre-commit -t commit-msg
+```
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-"""
-Main class for main things
-"""
+"""Main class for main things."""
 
 
 # This is a sample Python script.
@@ -8,34 +6,29 @@ Main class for main things
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+
 def very_nice_function() -> None:
-	"""
-	cool function
-	"""
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
-	print("hello world")
+    """Cool function."""
+    print("hello world")
+    print("hello world")
+    print("hello world")
+    print("hello world")
+    print("hello world")
+    print("hello world")
+    print("hello world")
+    print("hello world")
+    print("hello world")
+    print("hello world")
+    print("hello world")
 
 
-def print_hi2(name: str):
-	# Use a breakpoint in the code line below to debug your script.
-	print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def _print_hi2(name: str):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-	print_hi2('PyCharm')
+if __name__ == "__main__":
+    _print_hi2("PyCharm")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
