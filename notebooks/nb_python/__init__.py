d12ee56cae82d3326295e771ddee86ac6eacbade
Jonathan Zarecki <yonilx@gmail.com>
2021-07-05 00:29:58 +0300

    test: add matching notebooks and python files
    in order to test jupytext

---
+++ notebooks/nb_python/__init__.py
@@ -0,0 +1,13 @@
+# ---
+# jupyter:
+#   accelerator: GPU
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: notebooks//nb_python//py:percent,notebooks//notes//ipynb
+#     notebook_metadata_filter: all
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.3
+# ---
