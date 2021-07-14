2377f57945ee2799702a13bdccf481dbb72e4fbb
Jonathan Zarecki <yonilx@gmail.com>
2021-07-14 23:41:20 +0300

    chore: add longer function in order to check docstring hooks
    make f longer

--- module/class.py
+++ module/class.py
@@ -1,18 +1,40 @@
-"""
-Example class for nice things
-"""
+"""Example class for nice things."""
 
 
 class Tasdasdasdasdasdasdasdasd:
-	a = 1
-	b = 2
-	c = 3
+    a = 1
+    b = 2
+    c = 3
 
 
-sql = """
-select max(b) from test_table
+def f(sql: str) -> str:
+    """asdasdasdsa.
 
-where 1=1 and 2=2 group by a
+    Args:
+        sql:  asdasdas
+
+    Returns:
+        sql. asdasd.
+    """
+    sql = """
+    select max(b) from test_table
+
+    where 1=1 and 2=2 group by a
 
 
-"""
+    """
+    sql = """
+    select max(b) from test_table
+
+    where 1=1 and 2=2 group by a
+
+
+    """
+    sql = """
+    select max(b) from test_table
+
+    where 1=1 and 2=2 group by a
+
+
+    """
+    return sql
