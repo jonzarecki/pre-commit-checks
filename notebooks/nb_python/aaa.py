4f11319310aec68b1be492decec546b803687240
Jonathan Zarecki <yonilx@gmail.com>
2021-08-20 22:20:42 +0300

    fix: make jupytext config work again.
    also remove configs which triggers eradicate.

--- notebooks/nb_python/aaa.py
+++ notebooks/nb_python/aaa.py
@@ -1,33 +1,6 @@
-# ---
-# jupyter:
-#   accelerator: GPU
-#   jupytext:
-#     cell_metadata_filter: -all
-#     formats: notebooks//nb_python//py:percent,notebooks//notes//ipynb
-#     notebook_metadata_filter: all
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.3
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-#   language_info:
-#     codemirror_mode:
-#       name: ipython
-#       version: 2
-#     file_extension: .py
-#     mimetype: text/x-python
-#     name: python
-#     nbconvert_exporter: python
-#     pygments_lexer: ipython2
-#     version: 2.7.6
-# ---
-
 # %%
 a = 1
+print(a)
 print(a)
 
 # %%
